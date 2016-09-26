6cddf31eb728a248a653927f311f472c8a06ab8e
alexis <alex6683@hotmail.fr>
2016-09-26 21:21:30 +0200

    TP1 + Rendu Achevé

--- Sous-séquences_Maximales.c
+++ Sous-séquences_Maximales.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 //O(n³)
 int * Algo1(int * tab, int n) {
@@ -8,17 +9,16 @@ int * Algo1(int * tab, int n) {
     int sommeMax = 0 ;
 
     for(i=0 ; i<n ; i++) {
-        sommeCourant = 0 ;
+        //sommeCourant = 0 ;
         for(j=i+1 ; j<n ; j++) {
+            sommeCourant = 0 ;
             for(cmp=i ; cmp<=j ; cmp++) {
-                if(sommeMax <= sommeCourant + tab[cmp]) {
-                    sommeCourant += tab[cmp] ;
+                sommeCourant += tab[cmp] ;
+                if(sommeMax < sommeCourant) {
                     sommeMax = sommeCourant ;
                     bornes[0] = i ;
                     bornes[1] = cmp ;
                 }
-                else
-                    sommeCourant += tab[cmp] ;
             }
         }
     }
@@ -33,15 +33,13 @@ int * Algo2(int * tab, int n) {
     for(i=0 ; i<n ; i++) {
         sommeCourant = 0 ;
         for(j=i ; j<n ; j++) {
-            if(sommeMax <= sommeCourant + tab[j]) {
+            sommeCourant += tab[j] ;
+            if(sommeMax < sommeCourant) {
                 bornes[0] = i ;
                 bornes[1] = j ;
-                sommeCourant += tab[j] ;
                 sommeMax = sommeCourant ;
 
             }
-            else
-                sommeCourant += tab[j] ;
         }
     }
 
@@ -56,8 +54,8 @@ int * Algo3(int * tab, int deb, int fin) {
     int * SSeq = malloc(sizeof(int)*3) ;
 
     if(deb == fin) {
-        SSeq[0] = deb ;
-        SSeq[1] = fin ;
+        SSeq[0] = deb  ;
+        SSeq[1] = deb ;
         SSeq[2] = tab[deb] ;
         return SSeq ;
     }
@@ -79,7 +77,7 @@ int * Algo3(int * tab, int deb, int fin) {
     }
     int sommeD = tab[mil+1], borneSup = mil + 1 ;
     int sommeDmax = sommeD ;
-    for(i=mil+2 ; i<=fin ; i++) {
+    for(i=mil+2 ; i <= fin ; i++) {
         sommeD += tab[i] ;
         if(sommeD >= sommeDmax) {
             sommeDmax = sommeD ;
@@ -106,16 +104,20 @@ int * Algo4(int * tab, int n) {
     int * bornes = malloc(sizeof(int)*2) ;
 
     int cmp = 0, imax = 0;
-    while(tab[cmp] < 0 || cmp<n) {
-        if(tab[cmp] < tab[imax])
+
+
+    //--INIT-----------------------//
+    while(tab[cmp] < 0 && cmp < n) {
+        if(tab[cmp] > tab[imax])
             imax = cmp ;
         cmp++ ;
     }
-    if(tab[cmp] < 0) {
+    if(cmp == n) {
         bornes[0] = imax ;
         bornes[1] = imax ;
         return bornes ;
     }
+    //--INIT-----------------------//
 
     int i, borneInf = cmp ;
     int sommeMax = 0 ;
@@ -138,25 +140,54 @@ int * Algo4(int * tab, int n) {
 
 void affiche_tab(int * tab, int taille) {
     int i ;
-    for(i=0 ; i<taille-1 ; i++)
+    for(i=0 ; i<taille ; i++)
         printf("%d ", tab[i]) ;
-    printf("\t%d\n", tab[taille-1]) ;
+    printf("\n") ;
 }
 
+int * creerTabRand(int taille) {
+    //srand(time(NULL));
+    int * tab = malloc(sizeof(int)*taille) ;
+    int i ;
+    for(i=0 ; i<taille ; i++) {
+        tab[i] = rand()%(-101 - 100) -100 ;
+    }
+    return tab ;
+}
 
-int main (int argc, char *argv[]) {
+int main (int argc, char * argv[]) {
 
-    int tab[10] = {-10, 23, 600, -1000, -1, 300, 199, -2, 199, -10} ;
+    if(argc != 3) {
+        printf("ERROR\nUtilisation : \t%s\t[taille_tab]\t{1,2,3,4}\n", argv[0]) ;
+        exit(1) ;
+    }
 
-    int * sousSeq = Algo3(tab, 0, 9) ;
+    int * bornes = malloc(sizeof(int)*2) ;
+    int * bornesRec = malloc(sizeof(int)*3) ;
 
-    affiche_tab(sousSeq, 3);
+    int taille = atoi(argv[1]) ;
+
+    int * tab = creerTabRand(taille) ;
+    affiche_tab(tab, taille);
+
+    switch (atoi(argv[2])) {
+    case 1 : bornes = Algo1(tab, taille) ;
+        affiche_tab(bornes, 2);
+        break ;
+    case 2 : bornes = Algo2(tab, taille) ;
+        affiche_tab(bornes, 2);
+        break ;
+    case 3 : bornesRec = Algo3(tab, 0, taille-1) ;
+        affiche_tab(bornesRec, 3);
+        break ;
+    case 4 : bornes = Algo4(tab, taille) ;
+        affiche_tab(bornes, 2);
+        break ;
+    default : printf("ERROR\nUtilisation : \t%s\t[taille_tab]\t{1,2,3,4}\n", argv[0]) ;
+        exit(1) ;
+    }
 
     return 0 ;
 }
-
-
-
-
 
 
