563690d79c7b54cf7db254fe7e3e6c77c9a0e2f4
alexis <alex6683@hotmail.fr>
2016-10-09 21:29:59 +0200

    TP2 en cours

---
+++ 3-color-to-dimacs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+    int nb_sommets = atoi(argv[1]) ;
+    int nb_aretes = atoi(argv[2]) ;
+
+    int A[nb_aretes] ;
+
+    for(int i=4 ; i< argc ; i++) {
+
+    }
+
+
+}
